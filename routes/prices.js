991f55b79bcb7037a714747dd58fe09862bdc3ab
Rohith Thammaiah <thammaiahrohith@gmail.com>
2020-02-15 19:41:54 +0530

    :tada: Fetching prices from `mongoDB`

--- routes/prices.js
+++ routes/prices.js
@@ -2,11 +2,20 @@ const express = require('express');
 
 const router = express.Router();
 
+const Price = require('../models/Price');
+
 /**
  * A GET request to fetch coffee prices stored in mongoDB
  */
 router.get("/", (req, res) => {
-    res.send({status: "ok"})
+    Price.find()
+        .then((data) => {
+            res.send({status: "success", data: data})
+        })
+        .catch(err => {
+            console.log(err);
+            res.send({status: "failure"})
+        });
 });
 
 module.exports = router;
