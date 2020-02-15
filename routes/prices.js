887bafe51596c03be2de37cee17ed1759d8c0281
Rohith Thammaiah <thammaiahrohith@gmail.com>
2020-02-15 20:07:14 +0530

    :zap: Created an API `/prices/latest` to fetch the latest prices

--- routes/prices.js
+++ routes/prices.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const Price = require('../models/Price');
 
 /**
- * A GET request to fetch coffee prices stored in mongoDB
+ * A GET request to fetch all coffee prices
  */
 router.get("/", (req, res) => {
     Price.find()
@@ -16,6 +16,20 @@ router.get("/", (req, res) => {
             console.log(err);
             res.send({status: "failure"})
         });
+});
+
+/**
+ * A GET request to fetch the latest coffee price
+ */
+router.get("/latest", (req,res) => {
+   Price.find().sort({scrappedAt: -1}).limit(1)
+       .then((data) => {
+           res.send({status: "success", data: data})
+       })
+       .catch(err => {
+           console.log(err);
+           res.send({status: "failure"})
+       });
 });
 
 module.exports = router;
