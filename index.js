3925483a84a61e661e3880ef35413c3b3aeb090c
Rohith Thammaiah <thammaiahrohith@gmail.com>
2020-02-15 14:32:56 +0530

    :zap: Added `scrapeForPrices` API to scrape coffee price

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 const express = require('express');
+const cheerio = require('cheerio');
+const request = require('request');
 
 const app = express();
+const PORT = process.env.port || 3000;
+
+// Start to listening for requests
+app.listen(PORT);
+
+// Routes start here
+
+app.get("/scrapeForPrices", (req, res) => {
+    request('http://kpa.org.in/', (error, response, html) => {
+        if (!error && response.statusCode === 200) {
+            const $ = cheerio.load(html);
+
+            const finalJSONArray = [];
+            $('.carousel-inner').each((i, div) => {
+                const spices = [];
+                const spiceCosts = [];
+                $(div).find('h1').each((k, item) => {
+                    spices.push($(item).text());
+                });
+                $(div).find('h5').each((k, item) => {
+                    spiceCosts.push($(item).text());
+                });
+                for (i = 0; i < spices.length; i++) {
+                    finalJSONArray.push({"spice": spices[i], "spicesCost": spiceCosts[i], "scrappedAt": Date.now()})
+                }
+            });
+            res.send({status: 'success', data: finalJSONArray})
+        }
+    });
+});
