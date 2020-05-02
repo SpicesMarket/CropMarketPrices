1d7f4754db31080e55ab7081abde07769fb4b68b
Rohith Thammaiah <thammaiahrohith@gmail.com>
2020-05-02 10:16:29 +0530

    :zap: Added pepper in comment

--- routes/prices.js
+++ routes/prices.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const Price = require('../models/Price');
 
 /**
- * A GET request to fetch all coffee prices
+ * A GET request to fetch all coffee & pepper prices
  */
 router.get("/", (req, res) => {
     Price.find()
@@ -19,7 +19,7 @@ router.get("/", (req, res) => {
 });
 
 /**
- * A GET request to fetch the latest coffee price
+ * A GET request to fetch the latest coffee & pepper price
  */
 router.get("/latest", (req,res) => {
    Price.findOne({}).sort({scrappedAt: -1}).limit(1)
