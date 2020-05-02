6fc0b94264859a6a0382897d94919bd96e8470e9
Rohith Thammaiah <thammaiahrohith@gmail.com>
2020-05-02 10:49:14 +0530

    :new: Added comments in scrapper.js

--- scrape/scrapper.js
+++ scrape/scrapper.js
@@ -8,6 +8,10 @@ const Price = require('../models/Price');
 const router = express.Router();
 const PRICES_URL = "http://kpa.org.in/";
 
+const INCREASE = 1;
+const IDLE = 0;
+const DECREASE = -1;
+
 /**
  * A GET request to scrape latest prices of coffee and store it in mongoDB
  */
@@ -21,18 +25,25 @@ router.post("/", (req, res) => {
             $('.carousel-inner').each((i, div) => {
                 const spices = [];
                 const spiceCosts = [];
+
+                // Scrape for spice name
                 $(div).find('h1').each((k, item) => {
                     spices.push($(item).text());
                 });
+
+                // Scrape for spice cost
                 $(div).find('h5').each((k, item) => {
                     spiceCosts.push($(item).text());
                 });
+
+                // Add each value to a JSON array
                 for (i = 0; i < spices.length; i++) {
                     finalJSONArray.push({
                         "spiceName": spices[i],
                         "spiceCost": spiceCosts[i]
                     })
                 }
+
             });
             const priceWrapper = new Price({prices: finalJSONArray});
             priceWrapper.save()
